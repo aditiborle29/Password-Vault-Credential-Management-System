127ba90f5075713e2b4f8ed64c73e8ac844c68ac
aditiborle29 <aditiborle29@gmail.com>
2026-09-11 15:25:48 +0530

    Fix CORS for Render frontend

--- src/main/java/com/securevault/security/CorsConfig.java
+++ src/main/java/com/securevault/security/CorsConfig.java
@@ -15,14 +15,23 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
+
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "https://password-vault-credential-management-r5la.onrender.com"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"));
+
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
